563f75c6dbcb4bbd1bb6329895ca21349e593e4f
holymountaincos <emmalorentattoos@gmail.com>
2025-03-18 11:01:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This JavaScript can be expanded later to make more interactive choices.
+document.addEventListener('DOMContentLoaded', function() {
+  // Example of logging user choice if you want to track selections
+  console.log("User has entered the website.");
+});
